cceb270bb21589124f5e54ebe4089c05dc3743ff
wenkai <wenkai@hotmail.com>
2024-08-29 20:47:54 +0800

    backend setup 2

---
+++ backend/firebase/firebase.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const admin = require("firebase-admin");
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
+
+admin.initializeApp({
+	credential: admin.credential.cert(serviceAccount),
+	databaseURL: "https://techies.firebaseio.com",
+	storageBucket: "gs://techies.appspot.com",
+});
+
+const db = admin.firestore();
+const storage = admin.storage();
+const bucket = admin.storage().bucket();
+
+module.exports = { admin, db, storage, bucket };
