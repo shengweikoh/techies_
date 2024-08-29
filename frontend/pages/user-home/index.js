dab4bdb0c84f93e777de72715a58c5cc768cb9a2
shengweikoh <shengweikoh.2023@scis.smu.edu.sg>
2024-08-29 19:27:44 +0800

    set up first page and navbar

---
+++ frontend/pages/user-home/index.js
@@ -0,0 +1,11 @@
+import ResponsiveAppBar from "../../components/navbar/navbar";
+
+export default function Page() {
+    return (
+        <main>
+            <div>
+                <ResponsiveAppBar />
+            </div>
+        </main>
+    )
+}
