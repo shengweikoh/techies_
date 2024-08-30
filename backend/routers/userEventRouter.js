27bd7efddfb0f11832d4baf2d4d267c642bb3c75
Lee Min <min.lee.2023@scis.smu.edu.sg>
2024-08-30 16:51:51 +0800

    update backend controls

--- backend/routers/userEventRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const {getUserEvents} = require("../controllers/getUserEventsController.js/index.js");
-
-const router = express.Router();
-
-router.get('/', getUserEvents);
-
-module.exports = router;
