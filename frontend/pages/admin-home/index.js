b79a2f5eb8ac0de6712093b9dac36dcb086b9cef
shengweikoh <shengweikoh.2023@scis.smu.edu.sg>
2024-08-31 03:00:13 +0800

    Added API calls for admin-home AND user-home, and touched up stylings

--- frontend/pages/admin-home/index.js
+++ frontend/pages/admin-home/index.js
@@ -1,4 +1,4 @@
-import ResponsiveAppBar from "../../components/admin-navbar/navbar";
+import ResponsiveAppBar from "../../components/user-navbar/navbar";
 import React from 'react';
 import './page.css';
 import Link from "next/link";
@@ -6,61 +6,175 @@ import EventIcon from '@mui/icons-material/Event';
 import CreateIcon from '@mui/icons-material/Create';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
 import { Box } from "@mui/material";
+import { useState, useEffect } from 'react';
+import axios from "axios";
 
 export default function Page() {
+    const Dashboard = () => {
+        const [adminEvents, setAdminEvents] = useState(null);
+        const [eventsArray, setEventsArray] = useState([]);
+        const [error, setError] = useState(null);
 
-  const Card = ({ title, dateRange, location }) => {
-    return (
-        <div className="card">
-            <div className="card-content">
-                <h2>{title}</h2>
-                <Box display="flex" alignItems="center" gap="15px">
-                    <CalendarMonthIcon />
-                    <p>{dateRange}</p>
-                </Box>
-                <Box display="flex" alignItems="center" gap="15px">
-                    <LocationOnIcon />
-                    <p>{location}</p>
-                </Box>
-            </div>
-            <button className="card-button">View Event</button>
-        </div>
-    );
-};
+        const fetchEventsData = async () => {
+            try {
+                const userDocID = localStorage.getItem('userDocID');
+                if (!userDocID) {
+                    throw new Error('User document ID not found in localStorage');
+                }
+                const response = await axios.get(`http://localhost:8001/admin/adminEvent?adminID=${userDocID}`);
+                console.log('API Response:', response.data);
+                setAdminEvents(response.data);
+            } catch (error) {
+                setError(error.message);
+            }
+        };
+        
+        const fetchEventsArray = async () => {
+            try {
+                if (!adminEvents || !adminEvents.eventIDs) return;
+        
+                const adminEventsPromises = adminEvents.eventIDs.map(async (eventID) => {
+                    try {
+                        const response = await axios.get(`http://localhost:8001/controllers/ControllerEventDetail?eventID=${eventID}`);
+                        return response.data;
+                    } catch (err) {
+                        console.error(`Error fetching eventID ${eventID}:`, err);
+                        return null;
+                    }
+                });
+        
+                const resolvedEventsArray = await Promise.all(adminEventsPromises);
+                setEventsArray(resolvedEventsArray.filter(Boolean)); 
+            } catch (error) {
+                setError(error.message);
+                console.error("Error in fetchEventsArray:", error);
+            }
+        };
 
-const Dashboard = () => {
-    return (
-        <div className="dashboard">
+        useEffect(() => {
+            fetchEventsData();
+        }, []);
+
+        useEffect(() => {
+            if (adminEvents) {
+                fetchEventsArray();
+            }
+        }, [adminEvents]);
+
+      const renderEvents = () => {
+        if (!eventsArray || eventsArray.length == 0) {
+          return <p>No Events Available</p>
+        };
+        const cards = [];
+        for (let i = 0; i < eventsArray.length; i++) {
+          if (i >= 3) {
+            break;
+          }
+
+          cards.push(
+            <EventCard
+              key={i}
+              event={eventsArray[i]}
+            />
+          );
+        }
+
+        return (
+          <div className="dashboard-cards">
+            {error ? (
+              <p>{error}</p>
+            ) : eventsArray.length > 0 ? (
+              cards
+            ) : (
+              <p>No Available Events</p>
+            )}
+          </div>
+        );
+      }
+
+        return (
+          <div className="dashboard">
             <div className="dashboard-container">
-                <Box display="flex" alignItems="center" gap="15px">
-                    <EventIcon fontSize="large"/>
-                    <h2>My Events</h2>
-                </Box>
-                <p className="p-events">View your events</p>
-                <div className="dashboard-cards">
-                    <Card title="Singapore Night Festival 2024" dateRange="23 Aug 24 - 7 Sep 24" location="Bras Basah/Bugis" />
-                    <Card title="Halloween Horror Nights 2024" dateRange="30 Aug 24 - 3 Nov 24" location="Universal Studios Singapore" />
-                    <Card title="Christmas Wonderland 2024" dateRange="1 Dec 24 - 1 Jan 25" location="Gardens By The Bay" />
-                </div>
+              <Box display="flex" alignItems="center" gap="15px">
+                <EventIcon fontSize="large"/>
+                <h2>My Events</h2>
+              </Box>
+              <p>View the details of events</p>
+              <div className="dashboard-cards">
+                {renderEvents()}
+              </div>
 
-                <Box display="flex" alignItems="center" gap="15px">
+              <Box display="flex" alignItems="center" gap="15px">
                     <CreateIcon fontSize="large"/>
                     <h2>Create an Event</h2>
                 </Box>
-                <p>Organise a map for a new event</p>
-                <div className="dashboard-details">
-                </div>
+                <p>Organise a new event</p>
+                <br></br>
                 <Link href="/admin-create-event" className="dashboard-button">Create Event</Link>
             </div>
-        </div>
-    );
+          </div>
+        );
+      };
+
+const EventCard = ({ event }) => {
+  const startDateTime = new Date(event.startDateTime);
+  const endDateTime = new Date(event.endDateTime);
+
+  const formattedStartDate = startDateTime.toLocaleString('en-GB', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  });
+
+  const formattedEndDate = endDateTime.toLocaleString('en-GB', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  });
+
+  const formattedStartTime = startDateTime.toLocaleString('en-GB', {
+    hour: '2-digit',
+    minute: '2-digit'
+  });
+
+  const formattedEndTime = endDateTime.toLocaleString('en-GB', {
+    hour: '2-digit',
+    minute: '2-digit'
+  });
+
+  return (
+    <div className="card">
+        <h2>{event.eventName}</h2>
+        <Box display="flex" alignItems="center" gap="10px">
+          <CalendarMonthIcon />
+          <p>{formattedStartDate} - {formattedEndDate}</p>
+        </Box>
+        <Box display="flex" alignItems="center" gap="10px">
+          <AccessTimeIcon />
+          <p>{formattedStartTime} - {formattedEndTime}</p>
+        </Box>
+        <Box display="flex" alignItems="center" gap="10px">
+          <LocationOnIcon />
+          <p>{event.eventLocation}</p>
+        </Box>
+        <Box display="flex" alignItems="center" gap="10px">
+          <AttachMoneyIcon />
+          <p>{event.eventPrice}</p>
+        </Box>
+        <Box display="flex" alignItems="center">
+          <Link href="/admin-view-event" className="card-button">View Event</Link>
+        </Box>
+    </div>
+  );
 };
 
-return (
-    <main>
-        <ResponsiveAppBar />
-        <Dashboard />
-    </main>
-);
+    return (
+        <main>
+            <ResponsiveAppBar />
+            <Dashboard />
+        </main>
+    )
 }
--- frontend/pages/user-home/index.js
+++ frontend/pages/user-home/index.js
@@ -1,67 +1,67 @@
 import ResponsiveAppBar from "../../components/user-navbar/navbar";
 import React from 'react';
 import './page.css';
+import Link from "next/link";
 import EventIcon from '@mui/icons-material/Event';
 import CreateIcon from '@mui/icons-material/Create';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
 import { Box } from "@mui/material";
-
+import { useState, useEffect } from 'react';
+import axios from "axios";
 
 export default function Page() {
-  const Card1 = () => {
-    return (
-    <div className="card">
-        <h2>Singapore Night Festival 2024</h2>
-        <Box display="flex" alignItems="center" gap="15px">
-          <CalendarMonthIcon />
-          <p>23 Aug 24 - 7 Sep 24</p>
-        </Box>
-        <Box display="flex" alignItems="center" gap="15px">
-          <LocationOnIcon />
-          <p>Bras Basah/Bugis</p>
-        </Box>
-        <button className="card-button">View Event</button>
-    </div>
-    );
-};
-
-const Card2 = () => {
-    return (
-    <div className="card">
-        <h2>Halloween Horror Nights 2024</h2>
-        <Box display="flex" alignItems="center" gap="15px">
-          <CalendarMonthIcon />
-          <p>30 Aug 24 - 3 Nov 24</p>
-        </Box>
-        <Box display="flex" alignItems="center" gap="15px">
-          <LocationOnIcon />
-          <p>Universal Studios Singapore</p>
-        </Box>
-        <button className="card-button">View Event</button>
-    </div>
-    );
-};
-
-const Card3 = () => {
-    return (
-    <div className="card">
-        <h2>Christmas Wonderland 2024</h2>
-        <Box display="flex" alignItems="center" gap="15px">
-          <CalendarMonthIcon />
-          <p>1 Dec 24 - 1 Jan 25</p>
-        </Box>
-        <Box display="flex" alignItems="center" gap="15px">
-          <LocationOnIcon />
-          <p>Gardens By The Bay</p>
-        </Box>
-        <button className="card-button">View Event</button>
-    </div>
-    );
-};
-
 
     const Dashboard = () => {
+      const [eventsData, setEventsData] = useState(null);
+      const [error, setError] = useState(null);
+
+      const fetchEventsData = async () => {
+        try {
+            const response = await axios.get(`http://localhost:8001/user/home`);
+            console.log('API Response:', response.data);
+            setEventsData(response.data);
+        } catch (error) {
+            setError(error.message);
+        }
+      };
+
+        useEffect(() => {
+          fetchEventsData();
+        }, []);
+
+      const renderEvents = () => {
+        if (!eventsData) {
+          return <p>No Events Available</p>
+        };
+        const cards = [];
+        for (let i = 0; i < eventsData.events.length; i++) {
+          if (i >= 3) {
+            break;
+          }
+          cards.push(
+            <EventCard
+              key={i}
+              event={eventsData.events[i]}
+            />
+          );
+        }
+
+        return (
+          <div className="dashboard-cards">
+            {error ? (
+              <p>{error}</p>
+            ) : eventsData.events.length > 0 ? (
+              cards
+            ) : (
+              <p>No Available Events</p>
+            )}
+          </div>
+        );
+      }
+
         return (
           <div className="dashboard">
             <div className="dashboard-container">
@@ -71,24 +71,64 @@ const Card3 = () => {
               </Box>
               <p>Popular events currently in Singapore</p>
               <div className="dashboard-cards">
-                <Card1 />
-                <Card2 />
-                <Card3 />
+                {renderEvents()}
               </div>
-              
-              <Box display="flex" alignItems="center" gap="15px">
-                <CreateIcon fontSize="large"/>
-                <h2>Join an Event</h2>
-              </Box>
-              <p>Search for an event to join</p>
-              <div className="dashboard-details">
-                
-              </div>
-              <button className="dashboard-button">Book Now</button>
             </div>
           </div>
         );
       };
+
+const EventCard = ({ event }) => {
+  const startDateTime = new Date(event.startDateTime);
+  const endDateTime = new Date(event.endDateTime);
+
+  const formattedStartDate = startDateTime.toLocaleString('en-GB', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  });
+
+  const formattedEndDate = endDateTime.toLocaleString('en-GB', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  });
+
+  const formattedStartTime = startDateTime.toLocaleString('en-GB', {
+    hour: '2-digit',
+    minute: '2-digit'
+  });
+
+  const formattedEndTime = endDateTime.toLocaleString('en-GB', {
+    hour: '2-digit',
+    minute: '2-digit'
+  });
+
+  return (
+    <div className="card">
+        <h2>{event.eventName}</h2>
+        <Box display="flex" alignItems="center" gap="10px">
+          <CalendarMonthIcon />
+          <p>{formattedStartDate} - {formattedEndDate}</p>
+        </Box>
+        <Box display="flex" alignItems="center" gap="10px">
+          <AccessTimeIcon />
+          <p>{formattedStartTime} - {formattedEndTime}</p>
+        </Box>
+        <Box display="flex" alignItems="center" gap="10px">
+          <LocationOnIcon />
+          <p>{event.eventLocation}</p>
+        </Box>
+        <Box display="flex" alignItems="center" gap="10px">
+          <AttachMoneyIcon />
+          <p>{event.eventPrice}</p>
+        </Box>
+        <Box display="flex" alignItems="center">
+          <Link href="/user-view-event" className="card-button">View Event</Link>
+        </Box>
+    </div>
+  );
+};
 
     return (
         <main>
