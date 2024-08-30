371d9a9c08931824becc474de796ee1c5054b2b8
shengweikoh <shengweikoh.2023@scis.smu.edu.sg>
2024-08-30 17:47:49 +0800

    push

---
+++ frontend/pages/admin-view-event/index.js
@@ -0,0 +1,29 @@
+import ResponsiveAppBar from "../../components/admin-navbar/navbar";
+import React from 'react';
+import './page.css';
+import Link from "next/link";
+import EventIcon from '@mui/icons-material/Event';
+import { Box } from "@mui/material";
+
+export default function Page() {
+
+    const Dashboard = () => {
+        return (
+          <div className="dashboard">
+            <div className="dashboard-container">
+              <Box display="flex" alignItems="center" gap="15px">
+                <EventIcon fontSize="large"/>
+                <h2>Event Details</h2>
+              </Box>
+            </div>
+          </div>
+        );
+      };
+
+    return (
+        <main>
+            <ResponsiveAppBar />
+            <Dashboard />
+        </main>
+    )
+}
