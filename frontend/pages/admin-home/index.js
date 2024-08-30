ce2d55931c97e75ac9b28ca70e4d20586f12f9cc
shengweikoh <shengweikoh.2023@scis.smu.edu.sg>
2024-08-30 11:33:05 +0800

    Added icons and fixed card responsiveness for user-home, admin-home pages

--- frontend/pages/admin-home/index.js
+++ frontend/pages/admin-home/index.js
@@ -2,6 +2,11 @@ import ResponsiveAppBar from "../../components/admin-navbar/navbar";
 import React from 'react';
 import './page.css';
 import Link from "next/link";
+import EventIcon from '@mui/icons-material/Event';
+import CreateIcon from '@mui/icons-material/Create';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import { Box } from "@mui/material";
 
 export default function Page() {
 
@@ -9,8 +14,14 @@ export default function Page() {
         return (
         <div className="card">
             <h2>Singapore Night Festival 2024</h2>
-            <p>23 Aug 24 - 7 Sep 24</p>
-            <p>Bras Basah/Bugis</p>
+            <Box display="flex" alignItems="center" gap="15px">
+              <CalendarMonthIcon />
+              <p>23 Aug 24 - 7 Sep 24</p>
+            </Box>
+            <Box display="flex" alignItems="center" gap="15px">
+              <LocationOnIcon />
+              <p>Bras Basah/Bugis</p>
+            </Box>
             <button className="card-button">View Event</button>
         </div>
         );
@@ -20,8 +31,14 @@ export default function Page() {
         return (
         <div className="card">
             <h2>Halloween Horror Nights 2024</h2>
-            <p>30 Aug 24 - 3 Nov 24</p>
-            <p>Universal Studios Singapore</p>
+            <Box display="flex" alignItems="center" gap="15px">
+              <CalendarMonthIcon />
+              <p>30 Aug 24 - 3 Nov 24</p>
+            </Box>
+            <Box display="flex" alignItems="center" gap="15px">
+              <LocationOnIcon />
+              <p>Universal Studios Singapore</p>
+            </Box>
             <button className="card-button">View Event</button>
         </div>
         );
@@ -31,8 +48,14 @@ export default function Page() {
         return (
         <div className="card">
             <h2>Christmas Wonderland 2024</h2>
-            <p>1 Dec 24 - 1 Jan 25</p>
-            <p>Gardens By The Bay</p>
+            <Box display="flex" alignItems="center" gap="15px">
+              <CalendarMonthIcon />
+              <p>1 Dec 24 - 1 Jan 25</p>
+            </Box>
+            <Box display="flex" alignItems="center" gap="15px">
+              <LocationOnIcon />
+              <p>Gardens By The Bay</p>
+            </Box>
             <button className="card-button">View Event</button>
         </div>
         );
@@ -43,14 +66,22 @@ export default function Page() {
         return (
           <div className="dashboard">
             <div className="dashboard-container">
-              <h2>My Events</h2>
+              <Box display="flex" alignItems="center" gap="15px">
+                <EventIcon fontSize="large"/>
+                <h2>My Events</h2>
+              </Box>
               <p>View your events</p>
               <div className="dashboard-cards">
                 <Card1 />
                 <Card2 />
                 <Card3 />
               </div>
-              <h2>Create an Event</h2>
+
+              <Box display="flex" alignItems="center" gap="15px">
+                <CreateIcon fontSize="large"/>
+                <h2>Create an Event</h2>
+              </Box>
+              <p>Organise a map for a new event</p>
               <div className="dashboard-details">
               </div>
               <Link href="/admin-create-event"  className="dashboard-button">Create Event</Link>
