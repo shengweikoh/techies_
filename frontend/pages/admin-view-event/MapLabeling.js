62f5687b3985e4e8aea930be41e2e7478c366680
shengweikoh <shengweikoh.2023@scis.smu.edu.sg>
2024-09-02 00:05:23 +0800

    Revert "Added popup after edits to marker is made"
    This reverts commit 47453af385e1d4cafe93b23e39cb82f07fe69c6d.

--- frontend/pages/admin-view-event/MapLabeling.js
+++ frontend/pages/admin-view-event/MapLabeling.js
@@ -3,7 +3,8 @@ import { MapContainer, ImageOverlay, Marker, Popup } from 'react-leaflet';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import axios from 'axios';
-import { Box, Dialog, DialogActions, DialogContent, DialogTitle, TextField, Button, Snackbar, Alert } from '@mui/material';
+import { Box, Dialog, DialogActions, DialogContent, DialogTitle, TextField, Button } from '@mui/material';
+import Link from 'next/link';
 
 const createIcon = (color) => {
   return new L.DivIcon({
@@ -19,7 +20,6 @@ const MapLabeling = ({ eventDocID, imgURL }) => {
   const [error, setError] = useState(null);
   const [openPopup, setOpenPopup] = useState(false);
   const [email, setEmail] = useState('');
-  const [snackbarOpen, setSnackbarOpen] = useState(false);
 
   // Fetch markers from API
   const fetchMapMarkers = async () => {
@@ -75,7 +75,6 @@ const MapLabeling = ({ eventDocID, imgURL }) => {
         markers,
       });
       console.log('Markers successfully updated:', response.data);
-      setSnackbarOpen(true); // Show the snackbar when markers are updated
     } catch (error) {
       console.error('Error updating markers:', error);
     }
@@ -174,7 +173,7 @@ const MapLabeling = ({ eventDocID, imgURL }) => {
                   }}
                   placeholder="Wait Time (minutes)"
                 />
-                <h3 style={{ fontWeight: "normal" }}>minutes</h3>
+                <h3 style={{fontWeight:"normal"}}>minutes</h3>
               </Box>
             </li>
           ))}
@@ -186,7 +185,7 @@ const MapLabeling = ({ eventDocID, imgURL }) => {
         <button className='dashboard-button' onClick={handleAddStaffClick}>
           Add Staff
         </button>
-        <button onClick={updateMarkers} className='dashboard-button'>
+        <button onClick={() => { updateMarkers() }} className='dashboard-button'>
           Save
         </button>
       </Box>
@@ -212,17 +211,6 @@ const MapLabeling = ({ eventDocID, imgURL }) => {
           <Button onClick={handleAddStaff}>Continue</Button>
         </DialogActions>
       </Dialog>
-
-      {/* Snackbar for success message */}
-      <Snackbar
-        open={snackbarOpen}
-        autoHideDuration={6000}
-        onClose={() => setSnackbarOpen(false)}
-      >
-        <Alert onClose={() => setSnackbarOpen(false)} severity="success">
-          Updated Marker Details
-        </Alert>
-      </Snackbar>
     </div>
   );
 };
--- frontend/pages/staff-view-event/MapLabeling.js
+++ frontend/pages/staff-view-event/MapLabeling.js
@@ -3,7 +3,8 @@ import { MapContainer, ImageOverlay, Marker, Popup } from 'react-leaflet';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import axios from 'axios';
-import { Box, Snackbar, Alert } from '@mui/material';
+import Link from 'next/link';
+import { Box } from '@mui/material';
 
 const createIcon = (color) => {
   return new L.DivIcon({
@@ -17,7 +18,6 @@ const MapLabeling = ({ eventDocID, imgURL }) => {
   const [markers, setMarkers] = useState([]);
   const [bounds, setBounds] = useState([]);
   const [error, setError] = useState(null);
-  const [snackbarOpen, setSnackbarOpen] = useState(false);
 
   // Fetch markers from API
   const fetchMapMarkers = async () => {
@@ -73,7 +73,6 @@ const MapLabeling = ({ eventDocID, imgURL }) => {
         markers,
       });
       console.log('Markers successfully updated:', response.data);
-      setSnackbarOpen(true); // Show the snackbar when markers are successfully updated
     } catch (error) {
       console.error('Error updating markers:', error);
     }
@@ -146,7 +145,7 @@ const MapLabeling = ({ eventDocID, imgURL }) => {
                   }}
                   placeholder="Wait Time (minutes)"
                 />
-                <h3 style={{ fontWeight: "normal" }}>minutes</h3>
+                <h3 style={{fontWeight:"normal"}}>minutes</h3>
               </Box>
             </li>
           ))}
@@ -159,17 +158,6 @@ const MapLabeling = ({ eventDocID, imgURL }) => {
           Save
         </button>
       </div>
-
-      {/* Snackbar for success message */}
-      <Snackbar
-        open={snackbarOpen}
-        autoHideDuration={6000}
-        onClose={() => setSnackbarOpen(false)}
-      >
-        <Alert onClose={() => setSnackbarOpen(false)} severity="success">
-          Updated Marker Details
-        </Alert>
-      </Snackbar>
     </div>
   );
 };
