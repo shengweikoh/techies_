159b376c799b385c79f02bac6dd449c6070c86ee
wenkai <wenkai@hotmail.com>
2024-09-06 19:12:52 +0800

    backend update

--- backend/firebase/firebase.js
+++ backend/firebase/firebase.js
@@ -23,7 +23,8 @@
 
 require("dotenv").config();
 const admin = require("firebase-admin");
-const serviceAccount = require('./serviceAccountKey.json');
+//const serviceAccount = require('./serviceAccountKey.json');
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
 
 admin.initializeApp({
 	credential: admin.credential.cert(serviceAccount),
